f9035b3cc72e1892ab539434685f9f705942dddb
sieugene <eugenepoluakov@gmail.com>
2026-08-05 19:40:28 +0900

    feat : refactor parser, main script for creating parsed emails list

--- ch3/exercises/4/main.py
+++ ch3/exercises/4/main.py
@@ -1,19 +1,22 @@
 from utils.download_corpus import download_corpus
 from utils.email_parser_rfc5322 import EmailParser
 from glob import glob
-from os import path
+from pathlib import Path
 
 def main():
     download_corpus()
-    files = {"ham": ["easy_ham", "easy_ham_2", "hard_ham"], "spam": ["spam", "spam_2"]}
-    result = {}
-    for label, folders in files.items():
-        result[label] = 0
-        for file in files[label]:
-            folder = path.join("data", "downloads", file)
-            count = len(glob(f"{folder}/*"))
-            result[label] += count
-    print(result)
 
+    folder_cats = {"ham": ["easy_ham", "easy_ham_2", "hard_ham"], "spam": ["spam", "spam_2"]}
+    emails = []
+
+    for folderCat, _ in folder_cats.items():
+        for folderName in folder_cats[folderCat]:
+            folder = Path("data", "downloads", folderName)
+            files = [x for x in folder.iterdir() if x.is_file()]
+            for file in files:
+                email = EmailParser(file)
+                emails.append(email.getEmail())
+
+    print(emails)
 
 main()
--- ch3/exercises/4/utils/email_parser_rfc5322.py
+++ ch3/exercises/4/utils/email_parser_rfc5322.py
@@ -1,56 +1,81 @@
-
 from pathlib import Path
+from typing import TypedDict
+
+
+class Header(TypedDict):
+    key: str
+    values: list[str]
+
+
 class EmailParser:
-	def __init__(self, filepath):
-		self.filepath = filepath
-		self.lines = []
-		self.body_start_index = None
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.lines = []
+        self.body_start_index = None
 
-		self.lines = self.read().read_text().splitlines()
-		for i, line in enumerate(self.lines):
-			# empty line marks the start of mail content
-			if line == '':
-				self.body_start_index = i + 1
-				break
+        self.lines = self.__read().read_text().splitlines()
+        for i, line in enumerate(self.lines):
+            # empty line marks the start of mail content
+            if line == "":
+                self.body_start_index = i + 1
+                break
 
-	def read(self):
-		file_path = Path(self.filepath)
-		if file_path.exists():
-			return file_path
-		else:
-			print("File by path", self.filepath , "doesn't exist")
-			return None
+        self.headers = self.__parse_headers()
 
-	def parse_headers(self):
-		headers = []
-		parts = self.lines[:self.body_start_index]
+    def __read(self):
+        file_path = Path(self.filepath)
+        if file_path.exists():
+            return file_path
+        else:
+            print("File by path", self.filepath, "doesn't exist")
+            return None
 
-		for _, part in enumerate(parts):
-			if not part:
-				continue
-			if part.startswith((" ", "\t")):
-				if headers:
-					headers[-1]["values"][0] = "\n" + part
-				continue
-			key, *values = part.split(":")
-			if len(key.split()) == 1:
-				headers.append({"key": key, "values": values})
-		return headers
+    def __parse_headers(self) -> list[Header]:
+        headers: list[Header] = []
+        parts = self.lines[: self.body_start_index]
 
-	def getKeys(self):
-		headers = self.parse_headers()
-		keys = []
-		for header in headers:
-			keys.append(header["key"])
-		return keys
-	
-	def parse_body(self):
-		body_lines = self.lines[self.body_start_index:]
-		body = "\n".join(body_lines)
-		return body
+        for _, part in enumerate(parts):
+            if not part:
+                continue
+            if part.startswith((" ", "\t")):
+                if headers:
+                    headers[-1]["values"][0] += "\n" + part
+                continue
+            key, *values = part.split(":", 1)
+            if len(key.split()) == 1:
+                headers.append({"key": key, "values": values})
+        return headers
 
-		
+    def getKeys(self):
+        keys = []
+        for header in self.headers:
+            keys.append(header["key"])
+        return keys
 
-# emailCustom = EmailParser("./data/downloads/easy_ham/00001.7c53336b37003a9286aba55d2945844c")
-# print(emailCustom.parse_body())
-# print(emailCustom.getKeys())
+    def getData(self, key):
+        headers = self.__parse_headers()
+        findValues: list[str] | None = None
+
+        for header in headers:
+            if header["key"] == key:
+                findValues = header["values"]
+                break
+
+        if not findValues:
+            raise ValueError("Header not found")
+
+        return findValues
+
+    def parse_body(self):
+        body_lines = self.lines[self.body_start_index :]
+        body = "\n".join(body_lines)
+        return body
+
+    def getEmail(self):
+        return {
+            "body": self.parse_body(),
+            "from": self.getData("From"),
+            "to": self.getData("To"),
+            "subject": self.getData("Subject"),
+            "headers": self.headers,
+        }
